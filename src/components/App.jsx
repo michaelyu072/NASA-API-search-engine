efd435814867f50f352d7e9a97f7c098d4bc074a
Michael <apple@apples-MacBook-Pro.local>
2021-08-10 10:16:49 -0500

    initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="container">
+      <button className="add">Add</button>
+
+      <p className="score">2</p>
+    </div>
+  );
+}
+
+export default App;
