eb5f9ce33e71a6cb4af8f92a19ad67f1999a9c4a
MichaelYuVanderbilt <apple@apples-MacBook-Pro-2.local>
2021-12-12 20:29:00 -0600

    finished search bar color

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,13 @@
 import React from 'react';
-
+import SearchIcon from '@mui/icons-material/Search';
 
 function SearchBar() {
 
     return ( <>
         <div className = 'searchBarContainer'>
+        <div className = 'iconContainer'>
+        <SearchIcon/>
+        </div>
         <input className = 'searchBar'>
 
         </input>
