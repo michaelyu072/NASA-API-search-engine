dcc3564cd847ccfed7113f9fcf21f0fd10d4b1d4
MichaelYuVanderbilt <apple@apples-MacBook-Pro-2.local>
2021-12-26 12:45:16 -0600

    added text

---
+++ src/components/Heading.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+function Heading() {
+    return (
+    <p className = "heading">
+        NASA Image API Search Engine
+    </p>);
+}
+
+export default Heading;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,7 @@
 
 import React from 'react';
 import SearchBar from './SearchBar';
+import Heading from './Heading';
 
 function Main () {
 
@@ -9,7 +10,7 @@ function Main () {
     return (
         <section className = 'main'>
         <div className = 'searchContainer'>
-        {/* <Heading/> */}
+        <Heading/>
         <SearchBar/>
         {/* <ColorSelector/> */}
         </div>
