407e67bd631f37b33224d40828e970bfe92e0d86
MichaelYuVanderbilt <apple@apples-MacBook-Pro-2.local>
2021-12-30 06:16:18 -0600

    Updates

---
+++ precache-manifest.9cc40c2bdb7e9404298f02b39932f061.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "805f9ad0f331b111d696b5f91881fbba",
+    "url": "/NASA-API-search-engine/index.html"
+  },
+  {
+    "revision": "9987d4d315fbcdda18b5",
+    "url": "/NASA-API-search-engine/static/js/2.96974895.chunk.js"
+  },
+  {
+    "revision": "402cdc14200e40e0218c",
+    "url": "/NASA-API-search-engine/static/js/main.914f863a.chunk.js"
+  },
+  {
+    "revision": "5fa814704f97350e2e4a",
+    "url": "/NASA-API-search-engine/static/js/runtime-main.c9011797.js"
+  }
+]);
